2a8335569398b19a92ff6cadec0420c6e9828ffd
Joshua Skipper <yasoiskipper1@outlook.com>
2025-08-30 19:30:46 -0400

    Display top 5 successful SpaceX launches of 2022
    Updated query to filter and sort launches by date. Added user prompts for viewing detailed launch data, including success status and failure reasons. Enhanced user interaction for better experience.

--- SpaceXLaunchDataExploration/Program.cs
+++ SpaceXLaunchDataExploration/Program.cs
@@ -44,15 +44,37 @@ namespace SpaceXLaunchDataExploration
                     }
                     */
 
-                    var successLaunchesIn2022 = from item in launchJson
+                    var successLaunchesIn2022 = (from item in launchJson
                                                 where item.date_utc.Year == 2022 &
                                                 item.success == true
-                                              select item;
+                                                orderby item.date_utc descending
+                                              select item).Take(5);
 
+                    Console.WriteLine("TOP 5 MOST RECENT SUCCESSFUL SPACEX LAUNCHES OF 2022");
+                    Console.WriteLine($"----------------------------------------------------------\n");
                     foreach (var item in successLaunchesIn2022)
                     {
                         Console.WriteLine($"{item.name} - {item.date_utc}");
 
+                    }
+
+                    Console.WriteLine($"\nWould you like to see all the data from spaceX launches? (yes/no)");
+
+                    if (Console.ReadLine().Trim().ToLower() == "yes") 
+                    {
+                        foreach (var item in launchJson)
+                        {
+                            Console.WriteLine($"\nItem#:{item.launchpad} - Flight#:{item.flight_number} - Flight Name:{item.name} - Flight Success:{item.success} - Date:{item.date_utc} ");
+                            foreach (var fail in item.failures)
+                            {
+                                Console.WriteLine($"Failure Reason:{fail.reason}");
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exiting Program...");
                     }
                     
 
