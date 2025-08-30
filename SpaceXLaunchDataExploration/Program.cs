382008eaec0e4ad5b84fd3e3afad44e59b24b119
Joshua Skipper <yasoiskipper1@outlook.com>
2025-08-29 23:07:57 -0400

    Implement SpaceX API integration and data models

--- SpaceXLaunchDataExploration/Program.cs
+++ SpaceXLaunchDataExploration/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+namespace SpaceXLaunchDataExploration 
+{
+    class Program 
+    {
+        static  async Task Main(string[] args) 
+        {
+
+            //DateTime date = 8/29/2025
+            //var year = DateTime.Now.Year;
+
+
+            string url = "https://api.spacexdata.com/v4/launches?utm_source=chatgpt.com";
+
+            HttpClient client = new HttpClient();
+            //Console.WriteLine(year);
+
+            try 
+            {
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    var option = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    };
+
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    var launchJson = JsonSerializer.Deserialize<List<Root>>(json);
+
+                    /*
+                    foreach (var item in launchJson)
+                    {
+                        Console.WriteLine($"\nItem#:{item.launchpad} - Flight#:{item.flight_number} - Flight Name:{item.name} - Flight Success:{item.success} - Date:{item.date_utc} ");
+                        foreach(var fail in item.failures) 
+                        {
+                            Console.WriteLine($"Failure Reason:{fail.reason}");
+                        }
+
+                    }
+                    */
+
+                    var successLaunchesIn2022 = from item in launchJson
+                                                where item.date_utc.Year == 2022 &
+                                                item.success == true
+                                              select item;
+
+                    foreach (var item in successLaunchesIn2022)
+                    {
+                        Console.WriteLine($"{item.name} - {item.date_utc}");
+
+                    }
+                    
+
+                   
+                    
+                }
+
+            
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
